9c72c7e63d9bd1db8a42773877df9a943f2a7943
shun91 <shun9167@gmail.com>
2019-12-03 21:54:55 +0900

    ✨ added vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import Vue from "vue";
+import Vuetify from "vuetify";
 import App from "./App.vue";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
